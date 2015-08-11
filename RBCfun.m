13de30b92609d36148e583a4d3d3db433dd847c5
Anwesha Chaudhury <anweshachaudhury@Anwesha-Chaudhurys-MBP.local>
2015-08-10 22:51:08 -0400

    working RBCdyn

--- RBCfun.m
+++ RBCfun.m
@@ -5,11 +5,15 @@ function dP = RBCfun(t,P,fV,fH,L,J,birth,death,nV,nH,vGridU,vGridL,hGridL,hGridU
 % P=reshape(P,[nV,nH]);
 % JD = J';
 % keyboard
-dP = -J*P(:)+L*P(:)+birth(:)-death(:).*(P(:)>death(:));
+dP = J*P(:)+L*P(:)+birth(:).*sum(death(:).*P(:))/sum(birth(:))-death(:).*P(:); % .*sum(death(:).*P(:))/sum(birth(:))
+% dP1 = -1*(dP<0).*min(abs(dP),P)+dP.*(dP>=0);
 %  dP = -(JV.*(fV.*P) + JH.*(fH.*P))...
 %      +(Lv.*P+Lh.*P)+birth-death.*sign(P);
 %  dP = dP(:);
-t
+[sum(P),t]%,sum(death(:).*P(:))/sum(birth(:))]
+if sum(dP)<1e-2
+   return
+end
 %  keyboard
 end
 
